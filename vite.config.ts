8cb2198adab2a5595486755509d31fb20e5b9e70
trancore <ki.program0915@gmail.com>
2025-10-06 22:17:14 +0900

    feature: add vite-plugin-singlefile

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import vue from '@vitejs/plugin-vue';
 import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
+import { viteSingleFile } from 'vite-plugin-singlefile';
 import vueDevTools from 'vite-plugin-vue-devtools';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    viteSingleFile(),
+    tsconfigPaths({ loose: true }),
+  ],
   resolve: {
     alias: {
       '~': fileURLToPath(new URL('./src', import.meta.url)),
