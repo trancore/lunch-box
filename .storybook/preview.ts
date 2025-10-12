5b6587ad3715f8cc17c025717c171ba7431d16ea
trancore <ki.program0915@gmail.com>
2025-10-12 23:18:28 +0900

    feature: add promeVue settings in preview.ts

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,16 @@
-import type { Preview } from '@storybook/vue3-vite';
+import Aura from '@primeuix/themes/aura';
+import { type Preview, setup } from '@storybook/vue3-vite';
+import primeVue from 'primevue/config';
+import type { App } from 'vue';
+
+setup((app: App) => {
+  app.use(primeVue, {
+    theme: {
+      preset: Aura,
+      ripple: true,
+    },
+  });
+});
 
 const preview: Preview = {
   parameters: {
