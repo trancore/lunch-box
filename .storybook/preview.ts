d4a40ffb9de42f431f516e6cf0156fb43ab595aa
trancore <ki.program0915@gmail.com>
2025-10-13 14:38:45 +0900

    feature: add Icon com,ponent

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -3,6 +3,7 @@ import { type Preview, setup } from '@storybook/vue3-vite';
 import 'primeicons/primeicons.css';
 import primeVue from 'primevue/config';
 import type { App } from 'vue';
+import '~/assets/css/styles.scss';
 
 setup((app: App) => {
   app.use(primeVue, {
---
+++ src/components/Icon/index.stories.ts
@@ -0,0 +1,27 @@
+﻿import type { Meta, StoryObj } from '@storybook/vue3-vite';
+
+import Icon from './index.vue';
+
+const meta = {
+  component: Icon,
+  title: 'Component/Icon',
+} satisfies Meta<typeof Icon>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const ShoppingBagPrimary: Story = {
+  args: {
+    name: 'SHOPPING_BAG',
+    type: 'primary',
+    size: 2,
+  },
+};
+
+export const SearchSecondary: Story = {
+  args: {
+    name: 'SEARCH',
+    type: 'secondary',
+    size: 2,
+  },
+};
