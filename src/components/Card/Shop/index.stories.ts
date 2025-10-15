9c204af6101d9a3d622a88603f805b851829740b
trancore <ki.program0915@gmail.com>
2025-10-15 22:28:30 +0900

    feature add CardShop component

---
+++ src/components/Card/Shop/index.stories.ts
@@ -0,0 +1,31 @@
+﻿import type { Meta, StoryObj } from '@storybook/vue3-vite';
+
+import Shop from './index.vue';
+
+const meta = {
+  component: Shop,
+  title: 'Component/Card/Shop',
+  parameters: {
+    layout: 'centered',
+  },
+} satisfies Meta<typeof Shop>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Example: Story = {
+  args: {
+    imageUrl: '/src/assets/images/dummy-lunch.png',
+    name: 'Shop Title',
+    price: '1,500',
+    genre: 'イタリアン',
+    businessHours: '11:00 - 22:00',
+    rating: 4,
+  },
+};
+
+export const Undefined: Story = {
+  args: {
+    name: 'UndefinedShop',
+  },
+};
---
+++ src/components/Card/Shop/index.vue
@@ -0,0 +1,64 @@
+﻿<script setup lang="ts">
+type Props = {
+  // 画像URL
+  imageUrl?: string;
+  // 店舗名
+  name: string;
+  // ランチ値段
+  price?: string;
+  // ジャンル
+  genre?: string;
+  // 営業時間
+  businessHours?: string;
+  // 評価
+  rating?: number;
+};
+
+defineProps<Props>();
+</script>
+
+<template>
+  <Card class="shop-card">
+    <template #header>
+      <!-- TODO: no image -->
+      <Image
+        image-class="shop-image"
+        alt="shop"
+        :src="imageUrl ?? '/src/assets/images/no-image.png'"
+      />
+    </template>
+    <template #title>{{ name }}</template>
+    <template #subtitle>
+      <p class="subtitle">ランチ値段：~ {{ price ?? '??? ' }}円</p>
+    </template>
+    <template #content>
+      <p class="text-list">ジャンル：{{ genre ?? '???' }}</p>
+      <p class="text-list">営業時間：{{ businessHours ?? '???' }}</p>
+    </template>
+    <template #footer>
+      <Rating v-if="rating" :default-value="rating" readonly />
+    </template>
+  </Card>
+</template>
+
+<style lang="scss" scoped>
+.shop-card {
+  display: flex;
+  max-width: 270px;
+
+  ::v-deep .shop-image {
+    width: 100%;
+    object-fit: cover;
+    aspect-ratio: 4 / 3;
+  }
+}
+
+.subtitle {
+  margin: 0;
+  font-size: 0.9rem;
+}
+
+.text-list {
+  margin: 0;
+}
+</style>
