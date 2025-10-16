b84442c0391538e4de500b3692b3d6d1eec3c0ff
trancore <ki.program0915@gmail.com>
2025-10-16 23:28:51 +0900

    feature: add Footer component

---
+++ src/components/Footer/index.stories.ts
@@ -0,0 +1,15 @@
+﻿import type { Meta, StoryObj } from '@storybook/vue3-vite';
+
+import Footer from './index.vue';
+
+const meta = {
+  component: Footer,
+  title: 'Component/Footer',
+} satisfies Meta<typeof Footer>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {},
+};
---
+++ src/components/Footer/index.vue
@@ -0,0 +1,59 @@
+﻿<script setup lang="ts">
+import lunchBoxIcon from '~/assets/images/lunch-box_icon.svg';
+</script>
+
+<template>
+  <footer class="footer">
+    <div class="logo-area">
+      <div class="logo">
+        <component :is="lunchBoxIcon" class="logo-icon" />
+        <p class="logo-text">lunch-box</p>
+      </div>
+      <p class="description secondary">もう、悩まない</p>
+    </div>
+    <div class="link-area">
+      <h3 class="heading">Contact</h3>
+      <p>trancore</p>
+    </div>
+  </footer>
+</template>
+
+<style lang="scss" scoped>
+.footer {
+  display: flex;
+  justify-content: space-evenly;
+  height: 200px;
+  background-color: var(--p-surface-800);
+  color: var(--p-surface-200);
+
+  & > .logo-area {
+    display: flex;
+    flex-direction: column;
+    padding-top: 24px;
+
+    & > .logo {
+      display: flex;
+      align-items: center;
+      gap: 8px;
+
+      & > .logo-text {
+        margin: 0;
+        font-size: 1.5rem;
+        font-weight: bold;
+      }
+    }
+
+    & > .description {
+      margin-top: 4px;
+      color: var(--p-text-muted-color);
+    }
+  }
+
+  & > .link-area {
+    padding-top: 8px;
+    & > .heading {
+      margin: 32px 0 8px;
+    }
+  }
+}
+</style>
