0f9b4269f32cd428375215e4d0741f5c2433b430
trancore <ki.program0915@gmail.com>
2025-10-23 22:28:31 +0900

    feature: add Search page

--- src/components/Page/Search/index.vue
+++ src/components/Page/Search/index.vue
@@ -1,5 +1,33 @@
 ﻿<script setup lang="ts"></script>
 
-<template>search</template>
+<template>
+  <div class="search-page">
+    <div class="splash-area">
+      <ImageSplash
+        src="https://drive.google.com/thumbnail?id=1iMnuWgMBmR0us47IQQ1z-ldRIBfu2Nil"
+        alt="splash"
+        :height="150"
+      />
+    </div>
+    <div class="content">
+      <MenuSide>
+        <MenuResult />
+      </MenuSide>
+    </div>
+  </div>
+</template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.search-page {
+  > .splash-area {
+    position: relative;
+    text-align: center;
+  }
+
+  > .content {
+    margin: 0 auto;
+    padding: 44px 0;
+    max-width: $breakpoint-xl;
+  }
+}
+</style>
