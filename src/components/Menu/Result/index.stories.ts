6d432a526f3af1acf1d25cac5e5f962b5dc20728
trancore <ki.program0915@gmail.com>
2025-10-23 22:09:13 +0900

    feature: add MenuResult component

---
+++ src/components/Menu/Result/index.stories.ts
@@ -0,0 +1,15 @@
+﻿import type { Meta, StoryObj } from '@storybook/vue3-vite';
+
+import MenuResult from './index.vue';
+
+const meta = {
+  component: MenuResult,
+  title: 'Component/Menu/Result',
+} satisfies Meta<typeof MenuResult>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {},
+};
---
+++ src/components/Menu/Result/index.vue
@@ -0,0 +1,47 @@
+﻿<script setup lang="ts"></script>
+
+<template>
+  <div class="content">
+    <div class="title">
+      <Icon name="CARET_RIGHT" type="primary" :size="1.5"></Icon>
+      <p>お店を探す</p>
+    </div>
+    <div class="result">
+      <template v-for="(_value, index) in Array(10)">
+        <CardShop
+          :id="index.toString()"
+          :imageUrl="'https://drive.google.com/thumbnail?id=1Z7DyO3snqH7QPwlyvB8-qzT_IRr-pLzE'"
+          :name="'店舗名'"
+          :price="'1000'"
+          :genre="'イタリアン'"
+          :businessHours="'11:00 ~ 14:00'"
+          :rating="4"
+        />
+      </template>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.content {
+  background-color: white;
+  padding: 24px;
+  border-radius: 15px;
+  width: 100%;
+
+  .title {
+    display: flex;
+    align-items: center;
+    gap: 8px;
+    font-size: 1.5rem;
+    font-weight: bold;
+  }
+
+  .result {
+    display: grid;
+    grid-template: repeat(3, 1fr) / repeat(3, 1fr);
+    place-items: center;
+    gap: 16px;
+  }
+}
+</style>
