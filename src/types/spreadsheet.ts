14c3c4a04906dadbf9ba8d50d251766d45c22da3
trancore <ki.program0915@gmail.com>
2025-11-03 21:54:29 +0900

    feature: add spreadsheet types

---
+++ src/types/spreadsheet.ts
@@ -0,0 +1,24 @@
+﻿export type SheetValues = [
+  /** 店舗ID */
+  number,
+  /** URL */
+  string,
+  /** 店舗名 */
+  string,
+  /** ジャンル */
+  string,
+  /** 予算 */
+  number,
+  /** 開店時間 */
+  Date,
+  /** 閉店時間 */
+  Date,
+  /** レビュー */
+  number,
+  /** 紹介文 */
+  string,
+  /** 作成日 */
+  Date,
+  /** 更新日 */
+  Date,
+][];
