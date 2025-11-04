201db8ffc7ec53069fc69c5bce31edeb483bbbce
trancore <ki.program0915@gmail.com>
2025-11-04 21:57:33 +0900

    feature: add transform utils

---
+++ src/utils/transform.ts
@@ -0,0 +1,31 @@
+﻿import type { Rating } from '~/types/shop';
+
+export function transform() {
+  /**
+   * レーティング文字列を数値に変換する
+   *
+   * @param rating レーティング文字列
+   * @returns レーティング数値
+   * @example ☆☆☆☆☆ -> 0
+   */
+  function transfoemRatingToNumber(rating: Rating): number {
+    switch (rating) {
+      case '☆☆☆☆☆':
+        return 0;
+      case '★☆☆☆☆':
+        return 1;
+      case '★★☆☆☆':
+        return 2;
+      case '★★★☆☆':
+        return 3;
+      case '★★★★☆':
+        return 4;
+      case '★★★★★':
+        return 5;
+      default:
+        return 0;
+    }
+  }
+
+  return { transfoemRatingToNumber };
+}
