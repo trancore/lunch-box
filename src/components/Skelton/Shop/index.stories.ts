029245311d9751fd3224b65ecc48da9d6aa5a52c
trancore <ki.program0915@gmail.com>
2025-11-06 22:41:42 +0900

    feature: add Skelton component

---
+++ src/components/Skelton/Shop/index.stories.ts
@@ -0,0 +1,19 @@
+﻿import type { Meta, StoryObj } from '@storybook/vue3-vite';
+
+import SkeltonShop from './index.vue';
+
+const meta = {
+  component: SkeltonShop,
+  title: 'Component/Skelton/Shop',
+} satisfies Meta<typeof SkeltonShop>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  render: (args) => ({
+    components: { SkeltonShop },
+    template: '<div style="width: 270px"><SkeltonShop /></div>',
+    setup: () => ({ args }),
+  }),
+};
